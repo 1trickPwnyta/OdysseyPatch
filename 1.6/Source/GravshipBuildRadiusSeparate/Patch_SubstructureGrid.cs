563d61934bf99535a57a9a5860402857f719581a
1trickPwnyta <sarahpace37@gmail.com>
2026-04-26 18:36:04 -0700

    Add option to show separate build radii for gravship components

---
+++ 1.6/Source/GravshipBuildRadiusSeparate/Patch_SubstructureGrid.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace OdysseyPatch.GravshipBuildRadiusSeparate
+{
+    [HarmonyPatch(typeof(SubstructureGrid))]
+    [HarmonyPatch(nameof(SubstructureGrid.DrawSubstructureFootprint))]
+    public static class Patch_SubstructureGrid
+    {
+        private static int lastUpdateFrame = -1;
+
+        public static void Postfix()
+        {
+            if (OdysseyPatchSettings.GravshipBuildRadiusSeparate)
+            {
+                if (Time.frameCount == lastUpdateFrame)
+                {
+                    return;
+                }
+                lastUpdateFrame = Time.frameCount;
+
+                if (Find.DesignatorManager.SelectedDesignator is Designator_Place designator)
+                {
+                    CompProperties_SubstructureFootprint props = (designator.PlacingDef as ThingDef).comps.OfType<CompProperties_SubstructureFootprint>().FirstOrDefault();
+                    if (props != null)
+                    {
+                        GenDraw.DrawFieldEdges(GenRadial.RadialCellsAround(UI.MouseCell(), props.radius, true).ToList(), Color.white);
+                    }
+                }
+                foreach (Building building in Find.Selector.SelectedObjects.OfType<Building>())
+                {
+                    CompSubstructureFootprint comp = building.TryGetComp<CompSubstructureFootprint>();
+                    if (comp != null)
+                    {
+                        GenDraw.DrawFieldEdges(GenRadial.RadialCellsAround(building.Position, building.GetComp<CompSubstructureFootprint>().Props.radius, true).ToList(), Color.white);
+                    }
+                }
+            }
+        }
+    }
+}
