f40e6e199e3dd75339ee638332a6c918523559a0
1trickPwnyta <sarahpace37@gmail.com>
2026-05-01 17:34:14 -0700

    Fix bug with placing floors and with room stats gizmo

--- 1.6/Source/VacuumIntensityRoomStat/VacuumUtility.cs
+++ 1.6/Source/VacuumIntensityRoomStat/VacuumUtility.cs
@@ -4,6 +4,8 @@ namespace OdysseyPatch.VacuumIntensityRoomStat
 {
     public static class VacuumUtility
     {
+        public static Room statsRoom;
+
         public static bool HiddenOrHiddenVacuum(bool isHidden, RoomStatDef def, Room room) => isHidden || ((def.Worker is RoomStatWorker_VacuumIntensity r) && r.IsHidden(room));
     }
 }
